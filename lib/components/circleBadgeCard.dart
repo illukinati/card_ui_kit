7ccf5c1aeb58c75d2a3d3a353e690e9643b344e1
Eko Prasetyo <ekoprasetyolnh@gmail.com>
2018-08-23 16:29:05 +0900

    add ontap

--- lib/components/circleBadgeCard.dart
+++ lib/components/circleBadgeCard.dart
@@ -6,6 +6,7 @@ class CircleBadgeCard extends StatelessWidget {
   final String description;
   final Color descriptionColor;
   final Color backgroundColor;
+  final Function onTap;
 
   const CircleBadgeCard({
     Key key,
@@ -14,87 +15,91 @@ class CircleBadgeCard extends StatelessWidget {
     this.titleColor,
     this.title,
     this.description,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        color: (this.backgroundColor != null)
-            ? this.backgroundColor
-            : Colors.white,
-        margin: const EdgeInsets.all(10.0),
-        child: Stack(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
+      child: InkWell(
+        onTap: this.onTap,
+        child: Card(
+          color: (this.backgroundColor != null)
+              ? this.backgroundColor
+              : Colors.white,
+          margin: const EdgeInsets.all(10.0),
+          child: Stack(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 60.0,
+                      width: 100.0,
+                      child: Container(),
+                    ),
+                  ),
+                  Container(
                     height: 60.0,
-                    width: 100.0,
+                    width: 150.0,
+                    child: Image(
+                      fit: BoxFit.fitWidth,
+                      image: AssetImage("assets/images/circle.png"),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 60.0,
+                      width: 100.0,
+                      child: Container(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
                     child: Container(),
                   ),
-                ),
-                Container(
-                  height: 60.0,
-                  width: 150.0,
-                  child: Image(
-                    fit: BoxFit.fitWidth,
-                    image: AssetImage("assets/images/circle.png"),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 60.0,
-                    width: 100.0,
-                    child: Container(),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        (this.title != null) ? this.title : "",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15.0,
-                            color: (this.titleColor != null)
-                                ? this.titleColor
-                                : Colors.black),
-                      ),
-                      Text(
-                        (this.description != null) ? this.description : "",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w100,
-                          fontSize: 10.0,
-                          color: (this.descriptionColor != null)
-                              ? this.descriptionColor
-                              : Colors.grey,
+                  Container(
+                    margin: const EdgeInsets.only(top: 15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          (this.title != null) ? this.title : "",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                              color: (this.titleColor != null)
+                                  ? this.titleColor
+                                  : Colors.black),
                         ),
-                      ),
-                    ],
+                        Text(
+                          (this.description != null) ? this.description : "",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            fontSize: 10.0,
+                            color: (this.descriptionColor != null)
+                                ? this.descriptionColor
+                                : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(),
-                )
-              ],
-            ),
-          ],
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/components/easyMultipleCard.dart
+++ lib/components/easyMultipleCard.dart
@@ -10,6 +10,7 @@ class EasyMultipleCard extends StatelessWidget {
   final Color backgroundColor;
   final Color dividerColor;
   final List<Widget> items;
+  final Function onTap;
 
   const EasyMultipleCard(
       {Key key,
@@ -21,58 +22,62 @@ class EasyMultipleCard extends StatelessWidget {
       this.suffixIconColor,
       this.prefixIconColor,
       this.dividerColor,
-      this.items})
+      this.items,
+      this.onTap})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        margin: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      child: Text(
-                        (this.title != null) ? this.title : '',
-                        style: TextStyle(
-                            color: (this.titleColor != null)
-                                ? this.titleColor
-                                : Colors.black),
+      child: InkWell(
+        onTap: this.onTap,
+        child: Card(
+          margin: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: const EdgeInsets.all(15.0),
+                        child: Text(
+                          (this.title != null) ? this.title : '',
+                          style: TextStyle(
+                              color: (this.titleColor != null)
+                                  ? this.titleColor
+                                  : Colors.black),
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 10.0),
-                    child: (this.suffixIcon != null)
-                        ? Icon(
-                            this.suffixIcon,
-                            color: (this.suffixIconColor != null)
-                                ? this.suffixIconColor
-                                : Colors.black,
-                          )
-                        : Container(),
-                  ),
-                ],
+                    Container(
+                      margin: const EdgeInsets.only(right: 10.0),
+                      child: (this.suffixIcon != null)
+                          ? Icon(
+                              this.suffixIcon,
+                              color: (this.suffixIconColor != null)
+                                  ? this.suffixIconColor
+                                  : Colors.black,
+                            )
+                          : Container(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              height: 2.0,
-              color: (this.dividerColor != null)
-                  ? this.dividerColor
-                  : Colors.grey[200],
-            ),
-            Column(children: this.items),
-            Container(
-              margin: const EdgeInsets.only(bottom: 5.0),
-            ),
-          ],
+              Container(
+                height: 2.0,
+                color: (this.dividerColor != null)
+                    ? this.dividerColor
+                    : Colors.grey[200],
+              ),
+              Column(children: this.items),
+              Container(
+                margin: const EdgeInsets.only(bottom: 5.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
